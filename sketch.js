cc2d56a26525e697c6e237d81d0e8b8bd4214b1c
Ryan9876543 <128496613+Ryan9876543@users.noreply.github.com>
2023-03-21 18:27:49 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var box
+
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200,200,35,45);
+  box.shapeColor = "red"
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown (RIGHT_ARROW) ) {
+    box.x = box.x + 3
+     }
+
+     if (keyIsDown (LEFT_ARROW) ) {
+      box.x = box.x - 3
+       }
+
+if (keyIsDown (UP_ARROW) ) {
+      box.y = box.y - 3
+       }
+
+    if (keyIsDown (DOWN_ARROW) ) {
+    box.y = box.y + 3
+    }
+drawSprites();
+}
+
+ 
+
+
