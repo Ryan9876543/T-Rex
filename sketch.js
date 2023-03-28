08e1df450bae667a87abde165612cd755422c6f8
Ryan9876543 <128496613+Ryan9876543@users.noreply.github.com>
2023-03-28 19:13:12 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,54 @@
-var box
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+function preload() {
+    trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+    trex_collided = loadImage("trex_collided.png");
+    groundImage = loadImage("ground2.png")
+}
 
 function setup() {
-  createCanvas(400,400);
-  box = createSprite (200,200,35,45);
-  box.shapeColor = "red"
+    createCanvas(600, 200);
+
+    //create a trex sprite
+    trex = createSprite(50,160,20,50);
+    trex.addAnimation("running", trex_running);
+    trex.scale = 0.5;
+
+    //create a ground sprite
+    ground = createSprite(200,180,400,20);
+    ground.addImage("ground",groundImage);
+    ground.x = ground.width /2;
+    ground.velocityX = -4;
+
+    invisibleGround = createSprite (200,190,400,20)
+    invisibleGround.visible = false
 }
 
-function draw() 
-{
-  background(30);
-  if (keyIsDown (RIGHT_ARROW) ) {
-    box.x = box.x + 3
-     }
 
-     if (keyIsDown (LEFT_ARROW) ) {
-      box.x = box.x - 3
-       }
 
-if (keyIsDown (UP_ARROW) ) {
-      box.y = box.y - 3
-       }
+function draw() {
+    background(220);
 
-    if (keyIsDown (DOWN_ARROW) ) {
-    box.y = box.y + 3
+    console.log(trex.y)
+
+    //jump when the space button is pressed
+    if (keyDown("space") && trex.y >= 100) {
+        trex.velocityY = -10;
+        }
+
+    
+    //if (trex.y >= -10) {}
+
+    trex.velocityY = trex.velocityY + 0.8
+    
+
+    if (ground.x < 0) {
+         ground.x = ground.width / 2;
     }
-drawSprites();
+
+    
+
+    trex.collide(invisibleGround);
+    drawSprites();
 }
-
- 
-
-
